dddd065cdb14096cf17c8b053c292ae8e00d0947
Matt Clifford <matt.clifford@bristol.ac.uk>
2023-06-22 09:33:31 +0100

    percept losses

--- percept_loss/loss/losses.py
+++ percept_loss/loss/losses.py
@@ -1,4 +1,6 @@
 import torch.nn as nn
+from torchmetrics.image.lpip import LearnedPerceptualImagePatchSimilarity
+from torchmetrics.functional import structural_similarity_index_measure
 
 
 def cross_entropy():
@@ -8,3 +10,9 @@ def cross_entropy():
 def MSE():
     criterion = nn.MSELoss()
     return criterion
+
+def LPIPS():
+    return LearnedPerceptualImagePatchSimilarity(net_type='vgg')
+
+def SSIM():
+    return structural_similarity_index_measure
