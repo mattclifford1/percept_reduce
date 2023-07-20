acf8d310c31ac7805b10e82123d25cbf611ee728
Matt Clifford <matt.clifford@bristol.ac.uk>
2023-07-20 23:31:14 +0100

    some results

--- percept_loss/pipeline_IMAGENET64_TRAIN.py
+++ percept_loss/pipeline_IMAGENET64_TRAIN.py
@@ -2,12 +2,12 @@ from percept_loss.networks import IMAGENET64_AUTOENCODERS
 import percept_loss.pipeline.generic
 # run configs
 runs = {
-    'data_percent': [1, 0.5, 0.1, 0.01, 'uniform'],
     # 'data_percent': ['uniform', 0.5, 0.1],
     'loss': ['SSIM', 'MSE', 'LPIPS', 'MSSIM', 'DISTS', 'NLPD'],
     # 'loss': ['DISTS', 'NLPD'],
     # 'network': ['conv_small_z', 'conv_bigger_z', 'conv_big_z'],
     'network': ['standard'],
+    'data_percent': [1, 0.5, 0.1, 0.01, 'uniform'],
 }
 
 percept_loss.pipeline.generic.run(runs, IMAGENET64_AUTOENCODERS, epochs=30, batch_size=32, preload_data=True, dataset='IMAGENET64_TRAIN', validate_every=1)
