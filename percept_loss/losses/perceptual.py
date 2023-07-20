0df3cdc36a7e8522a299138726fa9a2f6bb380e4
Matt Clifford <matt.clifford@bristol.ac.uk>
2023-07-20 10:02:33 +0100

    added nlpd and dists

--- percept_loss/losses/perceptual.py
+++ percept_loss/losses/perceptual.py
@@ -1,6 +1,12 @@
+import warnings
+
 from torchmetrics.image.lpip import LearnedPerceptualImagePatchSimilarity
 from torchmetrics.functional import structural_similarity_index_measure
 from pytorch_msssim import ssim, ms_ssim, SSIM, MS_SSIM
+from DISTS_pytorch import DISTS as dists_original
+
+from .NLPD_torch.pyramids import LaplacianPyramid
+
 
 class sim_to_loss:
     def __init__(self, sim_metric):
@@ -23,3 +29,15 @@ def SSIM2():
 
 def MSSIM():
     return sim_to_loss(MS_SSIM(data_range=1, size_average=True, channel=3, win_size=1))
+
+def NLPD(nlpd_k=1):
+    with warnings.catch_warnings():    # we don't care about the warnings these give
+        warnings.simplefilter("ignore")
+        metric = LaplacianPyramid(nlpd_k)
+    return metric
+
+def DISTS():
+    with warnings.catch_warnings():    # we don't care about the warnings these give
+        warnings.simplefilter("ignore")
+        metric = dists_original()()
+    return metric
